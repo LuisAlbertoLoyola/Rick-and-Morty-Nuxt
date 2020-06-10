e548e45778308869704726f4b4f305eb930b4743
Luis Alberto Loyola <luisalbertoloyola@gmail.com>
2020-06-10 14:08:58 -0400

    Ejemplo Nuxt

---
+++ components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-footer
+    color="purple darken-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="purple lighten-4 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Inspire',
+        'List',
+        'Events',
+      ],
+    }),
+  }
+</script>
---
+++ pages/Events.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-layout column justify-center align-center>
+    <h1>Episodios</h1>
+
+    <v-btn flat nuxt class="mx-2" to="/inspire">
+      Cartelera
+    </v-btn>
+  </v-layout>
+</template>
